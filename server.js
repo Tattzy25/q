07d5af70b3a688689b8028319d1bc69072d0e1d9
tattzy25 <tattzy2025@gmail.com>
2026-05-06 11:01:20 -0700

    FUCK AI

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.json());
 
 const clients = new Set();
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/src/index.html'));
 
 app.get('/events', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
@@ -17,15 +17,18 @@ app.get('/events', (req, res) => {
 });
 
 app.post('/training', (req, res) => {
+  const model = req.body.model;
   for (const client of clients) {
-    client.write(`data: ${JSON.stringify({ state: 'training', model: req.body.model })}\n\n`);
+    client.write(`data: ${JSON.stringify({ message: 'Model ' + model + ' is currently Training', dot: 'orange' })}\n\n`);
   }
   res.sendStatus(200);
 });
 
 app.post('/complete', (req, res) => {
+  const model = req.body.model;
+  const url = req.body.url;
   for (const client of clients) {
-    client.write(`data: ${JSON.stringify({ state: 'complete', model: req.body.model, url: req.body.url })}\n\n`);
+    client.write(`data: ${JSON.stringify({ message: 'Congratulations, ' + model + ' is Well Fed and Battle Ready.', dot: 'green', url: url })}\n\n`);
   }
   res.sendStatus(200);
 });
